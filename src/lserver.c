ae46e7096c03de054e4a48ff04ed79467c186b55
Leandre <leandre.blanchard@epitech.eu>
2019-01-09 20:25:40 +0100

    clang-format

--- src/lserver.c
+++ src/lserver.c
@@ -4,22 +4,19 @@
 
 #include "lserver.h"
 
-static int lserver_setvalues(lserver_t *server, size_t ports_n,
-                             size_t client_buffer_size)
+static int lserver_setvalues(lserver_t *server, size_t ports_n, size_t client_buffer_size)
 {
   memset(server, 0, sizeof(*server));
   server->client_buffer_size = client_buffer_size;
   server->epoll = epoll_create1(EPOLL_CLOEXEC);
   if (server->epoll == -1)
     return (-1);
-  if (gtab_create(&server->clients, 8) == -1
-      || gtab_create(&server->listeners, ports_n) == -1)
+  if (gtab_create(&server->clients, 8) == -1 || gtab_create(&server->listeners, ports_n) == -1)
     return (-1);
   return (0);
 }
 
-int lserver_create(lserver_t *server, const uint16_t *ports, size_t size,
-                   size_t client_buffer_size)
+int lserver_create(lserver_t *server, const uint16_t *ports, size_t size, size_t client_buffer_size)
 {
   lclient_t *listener;
   struct epoll_event evt;
@@ -37,8 +34,7 @@ int lserver_create(lserver_t *server, const uint16_t *ports, size_t size,
     if (lsocket_server(&listener->socket, ports[i], 64) == -1)
       return (-1);
     if (gtab_sappend(&server->listeners, listener) == -1
-        || epoll_ctl(server->epoll, EPOLL_CTL_ADD, listener->socket.fd, &evt)
-                   == -1)
+        || epoll_ctl(server->epoll, EPOLL_CTL_ADD, listener->socket.fd, &evt) == -1)
       return (-1);
   }
   return (0);
